8209cff76d24da6932a3402c1db3e552669ab457
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2022-11-21 16:12:58 +0530

    update2

--- src/Contrller/CreateNewAccController.java
+++ src/Contrller/CreateNewAccController.java
@@ -1,168 +1,26 @@
 package Contrller;
-import db.DBConnection;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import java.io.IOException;
-import java.sql.*;
 
 public class CreateNewAccController {
-    public AnchorPane root;
-    public Button btnregister;
-    public PasswordField txt_New_Password;
-    public PasswordField txt_Confirm_Password;
-    public TextField txtusername;
-    public TextField txtemail;
-    public Label lblpasswordnotmatchtwo;
-    public Label lblid;
-    public Label lblpasswordnotmatchone;
-    public Label LblID;
-    public Button BtnBackID;
-
-    public void initialize() {
-        lblpasswordnotmatchone.setVisible(false);
-        lblpasswordnotmatchtwo.setVisible(false);
-        txtusername.setDisable(true);
-        txtemail.setDisable(true);
-        txt_New_Password.setDisable(true);
-        txt_Confirm_Password.setDisable(true);
-        btnregister.setDisable(true);
-        BtnBackID.setDisable(true);
-        LblID.setText("ID");
-    }
-
-    public void btnaddnewuseronaction(ActionEvent actionEvent) {
-        txtusername.setDisable(false);
-        txtemail.setDisable(false);
-        txt_New_Password.setDisable(false);
-        txt_Confirm_Password.setDisable(false);
-        btnregister.setDisable(false);
-        BtnBackID.setDisable(false);
-        txtusername.requestFocus();
-        autogenaratedID();
-
-    }
-
-    public void txtusernameonaction(ActionEvent actionEvent) {
-        txtemail.requestFocus();
+    public void btnregonaction(ActionEvent actionEvent) {
     }
 
     public void txtnewpasswordonaction(ActionEvent actionEvent) {
-        txt_Confirm_Password.requestFocus();
-
-    }
-
-    public void txtemailonaction(ActionEvent actionEvent) {
-        txt_New_Password.requestFocus();
-
     }
 
     public void txtconfirmpasswordonaction(ActionEvent actionEvent) {
-        register();
     }
 
-    public void btnregonaction(ActionEvent actionEvent) {
-        register();
-        txtusername.requestFocus();
+    public void txtusernameonaction(ActionEvent actionEvent) {
     }
 
-    public void register() {
-        String new_password = txt_New_Password.getText();
-        String confirm_password = txt_Confirm_Password.getText();
-
-        boolean equals = new_password.equals(confirm_password);
-        if (equals) {
-            txt_New_Password.setStyle("-fx-border-color: transparent");
-            txt_Confirm_Password.setStyle("-fx-border-color: transparent");
-            lblpasswordnotmatchone.setVisible(false);
-            lblpasswordnotmatchtwo.setVisible(false);
-
-            String id = LblID.getText();
-            String username = txtusername.getText();
-            String email = txtemail.getText();
-
-            Connection connection = DBConnection.getInstance().getConnection();
-            try  {
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into user values (?,?,?,?)");
-                preparedStatement.setObject(1,id);
-                preparedStatement.setObject(2,username);
-                preparedStatement.setObject(3,email);
-                preparedStatement.setObject(4,confirm_password);
-                int i = preparedStatement.executeUpdate();
-
-                if (i!=0){
-                    Alert alert=new Alert(Alert.AlertType.CONFIRMATION,"sucsees..");
-                    alert.showAndWait();
-
-                    Parent parent=FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
-                    Scene scene=new Scene(parent);
-                    Stage primarystage= (Stage) root.getScene().getWindow();
-                    primarystage.setScene(scene);
-                    primarystage.setTitle("Login form");
-                    primarystage.centerOnScreen();
-
-                }
-                else {
-                    Alert alert=new Alert(Alert.AlertType.ERROR,"Error..");
-                    alert.showAndWait();
-                }
-            }
-            catch (SQLException throwables) {
-               throwables.printStackTrace();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-        } else {
-            txt_New_Password.setStyle("-fx-border-color: red");
-            txt_Confirm_Password.setStyle("-fx-border-color: red");
-            txt_New_Password.requestFocus();
-            lblpasswordnotmatchone.setVisible(true);
-            lblpasswordnotmatchtwo.setVisible(true);
-        }
+    public void txtemailonaction(ActionEvent actionEvent) {
     }
 
-    public void btnBackOnaction(ActionEvent actionEvent) throws IOException {
-        Parent parent = FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
-        Scene scene = new Scene(parent);
-        Stage primarystage = (Stage) root.getScene().getWindow();
-        primarystage.centerOnScreen();
-        primarystage.setScene(scene);
-
+    public void btnaddnewuseronaction(ActionEvent actionEvent) {
     }
 
-    public void autogenaratedID() {
-        Connection connection = DBConnection.getInstance().getConnection();
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(" select id from user order by id desc limit 1");
-
-            boolean IsAvailable = resultSet.next();
-
-            if (IsAvailable) {
-                String oldID = resultSet.getString(1);
-                int length = oldID.length();
-                oldID=oldID.substring(1,oldID.length());
-                int intid = Integer.parseInt(oldID);//cover id into id format
-                intid=intid+1;
-                LblID.setText("U00"+intid);
-                if (intid<10){
-                    LblID.setText("U00"+intid);
-                } else if (intid<100) {
-                    LblID.setText("U0"+intid);
-                }
-                else {
-                    lblid.setText("U"+intid);
-                }
-            } else {
-                LblID.setText("U001");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+    public void btnBackOnaction(ActionEvent actionEvent) {
     }
 }
--- src/Contrller/LoginFormController.java
+++ src/Contrller/LoginFormController.java
@@ -1,89 +1,18 @@
 package Contrller;
 
-import db.DBConnection;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Objects;
-
-import static javafx.fxml.FXMLLoader.load;
 
 public class LoginFormController {
-    public Button txt_login_id;
-    public TextField txt_username_id;
-    public AnchorPane root;
-    public PasswordField txt_Password_id;
+    public void login_on_action(ActionEvent actionEvent) {
+    }
 
-
-    public void initialize(){
-    txt_login_id.requestFocus();
+    public void txt_password_onaction(ActionEvent actionEvent) {
     }
 
     public void txt_username_onaction(ActionEvent actionEvent) {
-        txt_Password_id.requestFocus();
     }
 
-    public void txt_password_onaction(ActionEvent actionEvent)throws IOException {
-   login();
+    public void hypercreatenewaccountonmouseclick(MouseEvent mouseEvent) {
     }
-
-    public void login_on_action(ActionEvent actionEvent) throws IOException {
-    login();
-    }
-
-    public void hypercreatenewaccountonmouseclick(MouseEvent mouseEvent) throws IOException {
-        Parent parent= load(this.getClass().getResource("../view/create_new_acc_Form.fxml"));
-        Scene scene = new Scene(parent);
-        Stage primarystage= (Stage) root.getScene().getWindow();
-        primarystage.setScene(scene);
-        primarystage.setTitle("Create New Form");
-        primarystage.centerOnScreen();
-    }
-
-public void login(){
-    String username = txt_username_id.getText();
-    String password = txt_Password_id.getText();
-    Connection connection = DBConnection.getInstance().getConnection();
-    try {
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from user where username = ? and confirm_password = ?");
-        preparedStatement.setObject(1,username);
-        preparedStatement.setObject(2,password);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()){
-
-        Parent parent=FXMLLoader.load(this.getClass().getResource("../view/ToDoForm.fxml"));
-        Scene scene=new Scene(parent);
-        Stage stage = (Stage) root.getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("ToDO Form");
-        stage.centerOnScreen();
-        }
-        else {
-            Alert alert = new Alert(Alert.AlertType.ERROR,"Invalid");
-            alert.showAndWait();
-            txt_Password_id.clear();
-            txt_username_id.clear();
-            txt_username_id.requestFocus();
-        }
-    }
-    catch (SQLException | IOException e) {
-        throw new RuntimeException(e);
-    }
-    }
-
 }
